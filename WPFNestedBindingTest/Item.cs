64b89021ecc0c734c45d1a2d7f86d39ca96cdf88
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2021-07-22 12:32:01 +0200

    dev

---
+++ WPFNestedBindingTest/Item.cs
@@ -0,0 +1,13 @@
+﻿namespace WPFNestedBindingTest
+{
+    public class Item : NotifyPropertyChanged
+    {
+        private int value;
+
+        public int Value
+        {
+            get => value;
+            set => Set(ref this.value, value);
+        }
+    }
+}
---
+++ WPFNestedBindingTest/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿namespace WPFNestedBindingTest
+{
+    public class MainViewModel : NotifyPropertyChanged
+    {
+        private Item item2;
+
+        public MainViewModel()
+        {
+            Item = new Item();
+            Item2 = new Item();
+        }
+
+        public RelayCommand InvalidUpdateValue => new RelayCommand(() => Item.Value++);
+
+        public RelayCommand InvalidOverrideValue => new RelayCommand(() => Item = new Item
+        {
+            Value = 666
+        });
+
+        public RelayCommand ValidUpdateValue => new RelayCommand(() => Item2.Value++);
+
+        public RelayCommand ValidOverrideValue => new RelayCommand(() => Item2 = new Item
+        {
+            Value = 666
+        });
+
+        public Item Item { get; set; }
+
+        public Item Item2
+        {
+            get => item2;
+            set => Set(ref item2, value);
+        }
+    }
+}
